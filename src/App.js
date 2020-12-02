f7f30661c6e857078edcf2a9a80b66f256fb6674
link26 <link26@list.ru>
2020-12-02 18:08:19 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen 6 :)</p>
+      <p>Start editing to see some magic happen 7 :)</p>
     </div>
   );
 }
